bf19ebb64cd4ca97763d6c6b1f36da4ef95f577b
Meirinf <meirin.f@gmail.com>
2017-03-01 20:18:48 +0100

    Implementada la camara en la Parte de receta Personalizada

--- app/src/main/java/layout/RecetaPerson.java
+++ app/src/main/java/layout/RecetaPerson.java
@@ -1,20 +1,39 @@
 package layout;
 
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import test.minevera.R;
+
+import static android.app.Activity.RESULT_OK;
+
 public class RecetaPerson extends Fragment {
 
-    public RecetaPerson() {
-        // Required empty public constructor
-    }
+    private ImageView recetaImage;
+    private Intent takePictureIntent;
 
+    public RecetaPerson() {}
+
+    private String pathFotoTemporal;
+    private static final int REQUEST_TAKE_PHOTO = 1;
+    private static View view;
 
 
     @Override
@@ -26,6 +45,72 @@ public class RecetaPerson extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_receta_person, container, false);
+        view = inflater.inflate(R.layout.fragment_receta_person, container, false);
+        recetaImage = (ImageView) view.findViewById(R.id.ImagenRecetas);
+
+        recetaImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+        return view;
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        pathFotoTemporal = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        try {
+
+            if (requestCode == REQUEST_TAKE_PHOTO) {
+                if (resultCode == RESULT_OK) {
+
+                    Glide.with(getContext()).load(pathFotoTemporal).into(recetaImage);
+                    //Falta guardar la Url en la base de datos.
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
     }
 }
