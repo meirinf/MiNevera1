b65d2a8f18f4870bedfa84bc2c7351aff6abc7e4
Meirinf <meirin.f@gmail.com>
2017-03-02 16:50:31 +0100

    Mapa sencillo implementado

--- app/src/main/java/test/minevera/Mapa.java
+++ app/src/main/java/test/minevera/Mapa.java
@@ -1,31 +1,43 @@
 package test.minevera;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 
-
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.MapView;
 
 
 public class Mapa extends Fragment {
 
+    private MapView map;
+    private View view;
+
+
     public Mapa() {
+        // Required empty public constructor
     }
 
-
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_mapa, container, false);
+        view = inflater.inflate(R.layout.fragment_mapa, container, false);
+        map = (MapView) view.findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPQUESTOSM);
+        map.setTilesScaledToDpi(true);
+
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
         return view;
     }
 }
